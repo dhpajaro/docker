b0cbc62c334b83f027e4b503caf13dfb4fdab91a
Daniel Pájaro <41458493+dhpajaro@users.noreply.github.com>
2024-08-29 17:31:47 -0500

    added new stacks (pi-hole, etc)

--- scripts/sync_trakt_to_plex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-pushd "$parent_path"
-compose_file="../compose/media_server/compose.yaml"
-trap 'docker compose -f $compose_file down plextraktsync;popd; exit 1' EXIT HUP INT TERM
-docker compose -f $compose_file run --rm plextraktsync sync && \
-docker compose -f $compose_file down plextraktsync
